f192a8ba38dcf4bd2f9b221be627ffbdc4150a37
soaebabdullah <38485488+soaebabdullah@users.noreply.github.com>
2021-12-05 17:29:17 +0600

    Add files via upload

---
+++ om_hospital_main/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Hospital Management',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'summery': 'Module for managing the Hospitals',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'website': 'odoomates.com',
+    'depends': [],
+    'demo': [],
+    'data': [
+
+    ],
+    'installable': True,
+    'applicating': True,
+    'auto_install': False,
+
+}
