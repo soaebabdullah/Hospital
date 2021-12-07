4de2b104179775a197165a2dc7405d79b5b3e5d4
soaebabdullah <38485488+soaebabdullah@users.noreply.github.com>
2021-12-07 17:09:29 +0600

    How to Add Chatter to Form View in Odoo
    How to Add Chatter to Form View in Odoo (09)

--- om_hospital_main/models/patient.py
+++ om_hospital_main/models/patient.py
@@ -9,6 +9,7 @@ class SaleOrderInherit(models.Model):
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Patient Record'
     _rec_name = 'patient_name'
 
