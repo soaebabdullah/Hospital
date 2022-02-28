d11be907aa418d9879b182a3f38c52c4b0eac5b9
Soaeb Abdullah <38485488+soaebabdullah@users.noreply.github.com>
2022-02-28 13:28:30 +0600

    How To Add Constraints For A Field in Odoo
    How To Add Constraints For A Field in Odoo(16)

--- om_hospital_main/models/patient.py
+++ om_hospital_main/models/patient.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
-
+# Inheriting the Sale Order Model and Adding New Field
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
@@ -13,22 +14,13 @@ class HospitalPatient(models.Model):
     _description = 'Patient Record'
     _rec_name = 'patient_name'
 
-    patient_name = fields.Char(string="Name", required=True, track_visibility ="always")
-    patient_age = fields.Integer('Age', track_visibility ="always")
-    test = fields.Text(string="Test Report")
-    image_1920 = fields.Image("Image")
-    image = fields.Binary(string="Patient Image")
-    name = fields.Char(string="Pathologist Name ")
-    name_seq = fields.Char(string= "Patient ID", required=True, copy=False, readonly=True,
-                           index=True, default=lambda self: _('New'))
-    gender = fields.Selection([
-        ('male', 'Male'),
-        ('fe_male', 'Female'),
-    ], default='male', string="Gender")
-    age_group = fields.Selection([
-        ('major', 'Major'),
-        ('minor', 'Minor'),
-    ], string="Age Group", compute='set_age_group', store=True)
+    @api.constrains('patient_age')
+    def check_age(self):
+        for rec in self:
+            if rec.patient_age <=5:
+                raise ValidationError(
+                    "Age Is Must Be  Greater Then 5"
+                )
 
     # Write Compute Field and its Function
     @api.depends('patient_age')
@@ -47,3 +39,21 @@ class HospitalPatient(models.Model):
 
         result = super(HospitalPatient, self).create(vals)
         return result
+
+    patient_name = fields.Char(string="Name", required=True, track_visibility="always")
+    patient_age = fields.Integer('Age', track_visibility="always")
+    test = fields.Text(string="Test Report")
+    image_1920 = fields.Image("Image")
+    image = fields.Binary(string="Patient Image")
+    name = fields.Char(string="Pathologist Name ")
+    name_seq = fields.Char(string="Patient ID", required=True, copy=False, readonly=True,
+                           index=True, default=lambda self: _('New'))
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('fe_male', 'Female'),
+    ], default='male', string="Gender")
+    age_group = fields.Selection([
+        ('major', 'Major'),
+        ('minor', 'Minor'),
+    ], string="Age Group", compute='set_age_group', store=True)
+
